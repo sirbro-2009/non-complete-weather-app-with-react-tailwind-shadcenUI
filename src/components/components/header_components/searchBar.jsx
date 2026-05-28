d69270c539dcdf6be073fb93f39c51effcd223eb
sirbro-2009 <zernikhmouhamed8@gmail.com>
2026-05-28 20:31:05 +0100

    edit search bar

--- src/components/components/header_components/searchBar.jsx
+++ src/components/components/header_components/searchBar.jsx
@@ -4,21 +4,18 @@ import {
   CommandItem,
   CommandGroup,
 } from "@/components/ui/command"
-import { Field } from "@/components/ui/field"
-import { Input } from "@/components/ui/input"
+import { Search } from "lucide-react"
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group"
 import SharProps from "@/components/components/Hooks/SharProps"
 import { useContext } from "react"
 import {getWeatherApi} from '@/function'
 import { useTranslation } from "react-i18next"
-export default function SearchBar(){
-let {res,setRes,setWetherProp,setCoordinates} = useContext(SharProps)
-let {t} = useTranslation()
-return(
-<Command className={`w-1/2 lg:w-1/8 mx-2 text-white bg-black outline-2 rounded-2xl`}>
-    <Field orientation="horizontal">
-      <Input type="search"  placeholder={t('search_placeholder')} autoFocus
-  className={`text-white text-center  rounded-lg text-2xl m-2`}
-onInput={(e)=>{
+let InputGroupDemo=  
+(e,setRes)=>{
 if(e.target.value.trim()){
 fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${e.target.value}&count=5&language=en&format=json&language=${localStorage.getItem("i18nextLng")||`en`}`)
 .then(
@@ -30,8 +27,27 @@ name:"No city! try search",
 id:null
 }]})
 }
-}} />
-    </Field>
+}
+
+export default function SearchBar(){
+let {res,setRes,setWetherProp,setCoordinates} = useContext(SharProps)
+let {t} = useTranslation()
+return(
+<Command className={`w-1/2 lg:w-1/8 mx-2 text-white bg-black outline-2 rounded-2xl`}>
+    <InputGroup className="max-w-xs">
+      <InputGroupInput placeholder={t('search_placeholder')} onInput={(e)=>{InputGroupDemo(e,setRes)}} />
+      <InputGroupAddon>
+        <Search />
+      </InputGroupAddon>
+      <InputGroupAddon align="inline-end">{res.results[0].id?res.results.length:`No`} results</InputGroupAddon>
+    </InputGroup>
+{/*INPUT */
+/*
+    <Field orientation="horizontal">
+      <Input type="search"  placeholder={t('search_placeholder')} autoFocus
+  className={`text-white text-center  rounded-lg text-2xl m-2`}
+onInput={()=>{InputGroupDemo(setRes)}} />
+    </Field>*/}
 <CommandGroup>
 <CommandList className={"max-h-17 overflow-scroll scroll-smooth "}>
 {
